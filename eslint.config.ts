223d49111f23d970be4555f703e75322bbea5a42
Vladislav Yakovlev <vlad309523@gmail.com>
2026-04-25 05:11:11 +0200

    chore: fix lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,28 +1,15 @@
+import eslintConfig from '@vlad-iakovlev/eslint-config'
 import { defineConfig, globalIgnores } from 'eslint/config'
-import eslintConfig from './src/index.js'
 
 export default defineConfig(
-  globalIgnores(['dist', 'prettier.config.js']),
+  globalIgnores(['coverage', 'dist', 'prettier.config.js']),
   eslintConfig.node,
   {
     languageOptions: {
       parserOptions: {
-        projectService: {
-          allowDefaultProject: ['eslint.config.ts'],
-        },
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
-    },
-  },
-  {
-    files: ['**/*.test.ts', 'testUtils/**/*'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/unbound-method': 'off',
     },
   },
 )
